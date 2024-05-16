c62973f08a048930865bb89073dd5aebab599979
Guilherme Silva <silva.gsg@gmail.com>
2024-05-17 00:25:10 +0100

    feat: Implement nav menu

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,26 +1,44 @@
+import { useState } from "react";
+
 import placeholder from "../../data/placeholder.json";
 
 import "./NavBar.styles.scss";
 
-/* import NavMenu from "./navMenu/NavMenu"; */
+import NavMenu from "./navMenu/NavMenu";
 
 function NavBar() {
+  const [active, setActive] = useState(false);
+  const [content, setContent] = useState({});
+
   const links = placeholder.navigation.links;
 
   return (
-    <header>
+    <header
+      onMouseEnter={() => setActive(true)}
+      onMouseLeave={() => setActive(false)}
+    >
       <nav>
         <ul className="nav__list">
           {links.map((item) => {
             return (
-              <li key={item.id}>
+              <li
+                className={
+                  active && content.id === item.id && "list__item--active"
+                }
+                key={item.id}
+                onMouseEnter={() => setContent(item)}
+              >
                 <a href="/">{item.title}</a>
-                {/* <NavMenu/> */}
               </li>
             );
           })}
         </ul>
       </nav>
+      {active && content ? (
+        <NavMenu active={active} content={content} />
+      ) : (
+        <></>
+      )}
     </header>
   );
 }
--- src/components/navBar/navMenu/NavMenu.js
+++ src/components/navBar/navMenu/NavMenu.js
@@ -1,5 +1,11 @@
-function NavMenu() {
-  return <div>NavMenu</div>;
+import "./NavMenu.styles.scss";
+
+function NavMenu({ content }) {
+  return (
+    <div className="nav__menu__wrapper">
+      <div className="nav__menu">{content.id}</div>
+    </div>
+  );
 }
 
 export default NavMenu;
